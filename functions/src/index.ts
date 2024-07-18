4bbed170989cd3ea8e1602d61d142c58fbdeb632
erenkarakoc <erenkarakoc@aol.com>
2024-07-19 02:22:55 +0300

    frontend functions and frontend test page added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,98 +1,204 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import {onRequest} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import cors from "cors";
 
 admin.initializeApp();
 const db = admin.firestore();
+const corsHandler = cors({origin: true});
 
-// Create a new chat session
-exports.createSession = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
+const getRootDomain = (hostname: string) => {
+  if (/^\d{1,3}(\.\d{1,3}){3}$/.test(hostname) || hostname === "localhost") {
+    return "Localhost";
   }
 
-  const {createdBy} = req.body;
-  const sessionRef = db.collection("chatSessions").doc();
-  const sessionData = {
-    createdBy,
-    messages: [],
-    createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    lastActive: admin.firestore.FieldValue.serverTimestamp(),
-  };
+  const parts = hostname.split(".");
+  if (parts.length > 2) {
+    return parts.slice(-2).join(".");
+  }
+  return hostname;
+};
 
-  await sessionRef.set(sessionData);
-  res.status(200).send({
-    message: "Session created successfully",
-    sessionId: sessionRef.id,
+// Create a new chat session
+exports.createSession = functions.https.onRequest((req, res) => {
+  corsHandler(req, res, async () => {
+    if (req.method !== "POST") {
+      res.status(405).send("Method Not Allowed");
+      return;
+    }
+
+    try {
+      const {userEmail} = req.body;
+
+      const referer = req.get("Referer") || "Unknown";
+      let domain = "Unknown";
+      const url = new URL(referer);
+      domain = getRootDomain(url.hostname);
+
+      const sessionRef = db.collection("chatSessions").doc();
+      const sessionData = {
+        userEmail,
+        active: true,
+        messages: [],
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        lastActive: admin.firestore.FieldValue.serverTimestamp(),
+        domain,
+      };
+
+      await sessionRef.set(sessionData);
+      res.status(200).send({
+        message: "Session created successfully",
+        sessionId: sessionRef.id,
+      });
+    } catch (error) {
+      res.status(500).send({error: "Internal Server Error"});
+    }
+  });
+});
+
+// Get session by userEmail
+exports.getSession = functions.https.onRequest(async (req, res) => {
+  corsHandler(req, res, async () => {
+    if (req.method !== "GET") {
+      res.status(405).send("Method Not Allowed");
+      return;
+    }
+
+    try {
+      const userEmail = req.query.userEmail;
+      if (!userEmail) {
+        res.status(400).send({error: "User email is required"});
+        return;
+      }
+
+      const snapshot = await db.collection("chatSessions")
+        .where("userEmail", "==", userEmail)
+        .where("active", "==", true)
+        .limit(1)
+        .get();
+
+      if (snapshot.empty) {
+        res.status(404).send({message: "No session found for this email"});
+        return;
+      }
+
+      const doc = snapshot.docs[0];
+      const sessionData = {id: doc.id, ...doc.data()};
+
+      res.status(200).send(sessionData);
+    } catch (error) {
+      console.error("Error fetching session:", error);
+      res.status(500).send({error: "Internal Server Error"});
+    }
   });
 });
 
 // Send a message
-exports.sendMessage = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
-  }
+exports.sendMessage = functions.https.onRequest((req, res) => {
+  corsHandler(req, res, async () => {
+    if (req.method !== "POST") {
+      res.status(405).send("Method Not Allowed");
+      return;
+    }
 
-  const {sessionId, sender, text} = req.body;
-  const sessionRef = db.collection("chatSessions").doc(sessionId);
+    try {
+      const {sessionId, sender, text} = req.body;
+      const sessionRef = db.collection("chatSessions").doc(sessionId);
 
-  const sessionSnapshot = await sessionRef.get();
-  if (!sessionSnapshot.exists) {
-    return res.status(404).send({
-      error: "Session not found",
-    });
-  }
+      const sessionSnapshot = await sessionRef.get();
+      if (!sessionSnapshot.exists) {
+        res.status(404).send({
+          error: "Session not found",
+        });
+        return;
+      }
 
-  const newMessage = {
-    id: Date.now().toString(),
-    sender,
-    text,
-    timestamp: admin.firestore.FieldValue.serverTimestamp(),
-  };
-  await sessionRef.update({
-    messages: admin.firestore.FieldValue.arrayUnion(newMessage),
-    lastActive: admin.firestore.FieldValue.serverTimestamp(),
-  });
+      const newMessage = {
+        id: Date.now().toString(),
+        sender,
+        text,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      };
 
-  res.status(200).send({
-    message: "Message sent successfully",
+      const currentMessages = sessionSnapshot.data();
+      const updatedMessages = currentMessages || [];
+      updatedMessages.push(newMessage);
+
+      await sessionRef.update({
+        messages: updatedMessages,
+        lastActive: admin.firestore.FieldValue.serverTimestamp(),
+      });
+
+      res.status(200).send({
+        message: "Message sent successfully",
+      });
+    } catch (error) {
+      res.status(500).send({error: error});
+    }
   });
 });
 
 // Receive messages
-exports.receiveMessages = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "GET") {
-    return res.status(405).send("Method Not Allowed");
-  }
+exports.receiveMessages = functions.https.onRequest((req, res) => {
+  corsHandler(req, res, async () => {
+    if (req.method !== "GET") {
+      res.status(405).send("Method Not Allowed");
+      return;
+    }
 
-  const {sessionId} = req.query;
-  const sessionRef = db.collection("chatSessions").doc(sessionId);
+    try {
+      const sessionId = req.query.sessionId as string;
+      if (!sessionId) {
+        res.status(400).send({
+          error: "Session ID is required",
+        });
+        return;
+      }
 
-  const sessionSnapshot = await sessionRef.get();
-  if (!sessionSnapshot.exists) {
-    return res.status(404).send({
-      error: "Session not found",
-    });
-  }
+      const sessionRef = db.collection("chatSessions").doc(sessionId);
+      const sessionSnapshot = await sessionRef.get();
+      if (!sessionSnapshot.exists) {
+        res.status(404).send({
+          error: "Session not found",
+        });
+        return;
+      }
 
-  res.status(200).send(sessionSnapshot.data());
+      res.status(200).send(sessionSnapshot.data());
+    } catch (error) {
+      res.status(500).send({error: "Internal Server Error"});
+    }
+  });
 });
+
+// Check inactive sessions
+exports.checkInactiveSessions =
+  functions.pubsub.schedule("every 30 minutes").onRun(async () => {
+    const now = admin.firestore.Timestamp.now();
+    const INACTIVITY_PERIOD = 30 * 60 * 1000;
+
+    try {
+      const sessionsRef = admin.firestore().collection("chatSessions");
+
+      const snapshot = await sessionsRef
+        .where("active", "==", true)
+        .where("lastActive", "<", now.toMillis() - INACTIVITY_PERIOD)
+        .get();
+
+      if (snapshot.empty) {
+        console.log("No inactive sessions found.");
+        return;
+      }
+
+      const batch = admin.firestore().batch();
+
+      snapshot.forEach((doc) => {
+        const sessionRef = sessionsRef.doc(doc.id);
+        batch.update(sessionRef, {active: false});
+      });
+
+      await batch.commit();
+      console.log("Inactive sessions updated successfully.");
+    } catch (error) {
+      console.error("Error updating inactive sessions:", error);
+    }
+  });
