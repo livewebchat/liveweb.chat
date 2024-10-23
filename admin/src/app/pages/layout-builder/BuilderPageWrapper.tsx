558e97eed3581f91d5f96bdc9b05668a5d5f1cb9
livewebchat <erenkarakocw@gmail.com>
2024-10-23 16:43:46 +0300

    Changes from server

--- admin/src/app/pages/layout-builder/BuilderPageWrapper.tsx
+++ admin/src/app/pages/layout-builder/BuilderPageWrapper.tsx
@@ -1,6 +1,6 @@
-import {FC} from 'react'
-import {PageTitle} from '../../../_metronic/layout/core'
-import {BuilderPage} from './BuilderPage'
+import { FC } from "react"
+import { PageTitle } from "../../../_metronic/layout/core"
+import { BuilderPage } from "./BuilderPage"
 
 const BuilderPageWrapper: FC = () => {
   return (
